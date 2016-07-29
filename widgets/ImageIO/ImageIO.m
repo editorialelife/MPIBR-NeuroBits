5307e706ab4a5d2c3d67327144479dc86f98f1a2
Masneri Stefano <stocastico@gmail.com>
2016-07-29 10:12:11 +0200

    update .gitignore / work on ImageIO

--- widgets/ImageIO/ImageIO.m
+++ widgets/ImageIO/ImageIO.m
@@ -7,17 +7,19 @@ classdef ImageIO < handle
     
     %% --- Metadata Properties --- %%%
     properties
-        fileName
-        data
+        fileName = '';          % Image filename
+        fileFullPath = '';      % Full filename, together with absolute path
+        fileFolder = '';        % Folder containing the image
+        data = [];              % TODO Do we really want to have the data???
         
-        height
-        width
-        channels
-        stacks
-        time
-        tile
+        height = nan;           % Image height / number of rows
+        width = nan;            % Image width / number of columns
+        channels = nan;         % Number of image channels
+        stacks = nan;           % Image stacks (Z axis)
+        time = nan;             % Image timeseries
+        tile = nan;             % Image tiles per stack
         
-        data_type
+        data_type = '';         % Image datatype (uint8, int16, ...)
         
         scale_size %x,y,z,t
         scale_units
@@ -28,23 +30,61 @@ classdef ImageIO < handle
         time_frame
         time_stack
         
-        zoom
-        gain
+        zoom = nan;
+        gain = nan;
         
-        wavelengthExc
-        wavelengthEm
+        wavelengthExc = nan;
+        wavelengthEm = nan;
         refraction_media
-        NA
+        NA = nan;               % Numerical aperture
         
-        microscope_name
-        microscope_type
+        microscope_name = '';
+        microscope_type = '';
         
         objective_magnification
-        objective_name
-        
+        objective_name 
+    end
+    
+    properties (Constant = true)
+      VERSION = '0.1'
     end
     
     methods
+      function obj = ImageIO(filename)
+      %IMAGEIO ImageIo Constructor
+      %Performs basic assignments common to all classes
+      % INPUT
+      %   filename: The file to open, a char array
+      % OUTPUT
+      %   obj: the returned ImageIO object
+      % SEE ALSO
+      %   readBIO
+      
+        p = inputParser;
+        p.addRequired('filename', @(x) ischar(x))
+        
+        p.parse(filename);
+      end
+      
+      function [major, minor] = getVersion(object)
+      %GETVERSION Gets the class version
+      %Returns the class version as two parameters one is the major
+      %(used for releases) and one is the minor, used for small incremental
+      %improvements
+        C = strsplit(object.VERSION, '.');
+        if length(C) ~= 2
+          error('ImageIO.getVersion: Error parsing version string')
+        end
+        major = str2num(C{1});
+        minor = str2num(C{2});
+      end
+    end
+    
+    methods (Abstract = true)
+    % Here we have the methods that each subclass MUST implement
+    end
+    
+    methods (Access = protected)
     end
     
 end
