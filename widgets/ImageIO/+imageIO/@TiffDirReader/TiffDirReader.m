c4cfd77a2ab39be8a606cc669ee88fe0a0dce869
Masneri Stefano <stocastico@gmail.com>
2016-11-30 09:30:52 +0100

    Add file TiffDirReader

---
+++ widgets/ImageIO/+imageIO/@TiffDirReader/TiffDirReader.m
@@ -0,0 +1,20 @@
+classdef TiffDirReader
+  %TIFFDIRREADER Reads a collection of images from a folder
+  %   This class creates an image from a collection of pictures in a
+  %   folder. The class considers only tiff files inside the folder and
+  %   inspects the filenames to extract information about what each file
+  %   represents. 
+  %
+  %   Author: Stefano.Masneri@brain.mpge.de
+  %   Date: 30.11.2016
+  %   SEE ALSO: imageIO.TiffDirReader.TiffDirReader, imageIO.ImageIO, 
+  %     imageIO.TiffReader, imageIO.TiffWriter, Tiff
+  
+  properties
+  end
+  
+  methods
+  end
+  
+end
+
