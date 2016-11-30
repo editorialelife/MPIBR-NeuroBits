4e61970bccf04520104cd3f13406197aeab6de67
Masneri Stefano <stocastico@gmail.com>
2016-11-30 10:53:59 +0100

    TiffDirReader: Add help

--- widgets/WidgetImageBrowser/submodules/ImageIO/+imageIO/@TiffDirReader/TiffDirReader.m
+++ widgets/WidgetImageBrowser/submodules/ImageIO/+imageIO/@TiffDirReader/TiffDirReader.m
@@ -1,4 +1,4 @@
-classdef TiffDirReader
+classdef TiffDirReader < ImageIO
   %TIFFDIRREADER Reads a collection of images from a folder
   %   This class creates an image from a collection of pictures in a
   %   folder. The class considers only tiff files inside the folder and
@@ -11,9 +11,36 @@ classdef TiffDirReader
   %     imageIO.TiffReader, imageIO.TiffWriter, Tiff
   
   properties
+    tiffPtrs;       % array of pointers to tiff files
+    filePattern;    % string representing the file pattern
   end
   
   methods
+    function obj = TiffDirReader(folder, filePattern, dimensionOrder)
+    %TIFFDIRREADER Class constructor
+    %The constructor checks at first if the user provided a directory as argument. 
+    % If no directory is passed, a gui asks the user to select a directory.
+    % The user can also specify a file pattern, which will later be used to 
+    % choose the appropriate number of channels, Z stacks and so on. If no
+    % pattern is provided, the class will assume that the folder will
+    % contain images representing a volume containing as many Z slices as
+    % files, with just one channel and one tile. 
+    % INPUT
+    %   folder: the folder containing tiff files 
+    %   filePattern: the pattern used by the images. For example, if the
+    %     folder contains files like 'img_0001.tif', 'img_0002.tif' and so on, 
+    %     the file pattern will be 'img_%04d.tif'. A more complicated
+    %     pattern could be 'img_UII%02dX%02d_%02d_xyz-Table_%04d.ome.tif',
+    %     where there are four number representing the X/Y tile position,
+    %     the channel and the Z value.
+    %   dimensionOrder: the order of the dimensions presented in the file
+    %     pattern. If not specified, the value depends on the number of
+    %     format tags in the file pattern. Valid values could be 'Z',
+    %     'XYCZ', 'T'
+    % OUTPUT
+    %  obj: the TiffDirReader object
+    
+    end
   end
   
 end
