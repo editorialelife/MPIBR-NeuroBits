20aed009b464e69da58fa6e42c9a3ea9bec30399
Masneri Stefano <stocastico@gmail.com>
2016-12-05 16:07:36 +0100

    BioReader: put method readMetadata in separate file. Add testLSM file

---
+++ widgets/WidgetImageBrowser/submodules/ImageIO/tests/testLSM.m
@@ -0,0 +1,43 @@
+% TEST LSM FILES
+
+%% INPUT DATA
+if ispc
+  lsmFolder = '\\storage.corp.brain.mpg.de\data\Projects\ImageIO\TestDataFormats\ZeissLSM';
+else
+  lsmFolder = '/Volumes/data/Projects/ImageIO/TestDataFormats/ZeissLSM';
+end
+
+%% TEST CZI DATA - READ ALL DATA
+lsmFiles = {'2Positions.lsm', '2x2Tiles.lsm', '5Z.lsm', '5Z10T2x2Tiles2Pos.lsm', '10T.lsm'};
+
+%% EXAMPLE FILES - READ ALL THE DATA
+for k = 1:5
+  filename = lsmFiles{k};
+  fullPath = fullfile(lsmFolder, filename);
+  lsmFile = imageIO.BioReader(fullPath);
+  lsmData = lsmFile.read();
+  if ismatrix(lsmData)
+    imshow(imadjust(lsmData))
+    pause(0.5)
+  elseif 3 == ndims(lsmData)
+    for m = 1:size(lsmData, 3)
+      imshow(imadjust(lsmData(:,:,m)))
+      pause(0.5)
+    end
+  elseif 4 == ndims(lsmData)
+    for m = 1:size(lsmData, 4)
+      imshow(imadjust(lsmData(:,:,1,m)))
+      pause(0.5)
+    end
+  elseif 5 == ndims(lsmData)
+    for m = 1:size(lsmData, 5)
+      imshow(imadjust(lsmData(:,:,1,1,m)))
+      pause(0.5)
+    end
+  else % 6D
+    for m = 1:size(lsmData, 6)
+      imshow(imadjust(lsmData(:,:,1,1,m)))
+      pause(0.5)
+    end
+  end  
+end
