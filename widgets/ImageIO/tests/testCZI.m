ffdabdaf55f8144f190efc1b59b3e3a93356a797
stocastico <stocastico@gmail.com>
2016-12-05 11:44:57 +0100

    testCZI fix filename

--- widgets/ImageIO/tests/testCZI.m
+++ widgets/ImageIO/tests/testCZI.m
@@ -8,13 +8,13 @@ else
 end
 
 %% TEST CZI DATA - READ ALL DATA
-cziFiles = {'2Positions.czi, 2x2Tiles.czi', '5Z.czi', '5Z10T2x2Tiles2Pos.czi', ...
+cziFiles = {'2Positions.czi', '2x2Tiles.czi', '5Z.czi', '5Z10T2x2Tiles2Pos.czi', ...
   '10T.czi', 'Slide02_10x_RGT_Shading Correction.czi', ...
   'Slide03_10x_RGT_Shading Correction_StitchZenBlue.czi', ...
   ['ProblematicFiles' filesep() 'GreenSlide_Stack_1p-405_2p-720_exc561.czi']};
 
 %% EXAMPLE FILES - READ ALL THE DATA
-for k = 1:4
+for k = 1:5
   filename = cziFiles{k};
   fullPath = fullfile(cziFolder, filename);
   cziFile = imageIO.CZIReader(fullPath);
@@ -39,6 +39,6 @@ for k = 1:4
 end
 
 %% OTHER FILES - GET A SUBSET OF THE DATA
-for k = 5:length(cziFiles)
+for k = 6:length(cziFiles)
 
 end
