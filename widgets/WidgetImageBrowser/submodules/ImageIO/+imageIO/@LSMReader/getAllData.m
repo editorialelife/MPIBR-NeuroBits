e5c8253f6760dc8895323871161c8bddfb4374de
Masneri Stefano <stocastico@gmail.com>
2017-02-08 12:28:07 +0100

    Add skeleton of getAllData function for LSMReader

---
+++ widgets/WidgetImageBrowser/submodules/ImageIO/+imageIO/@LSMReader/getAllData.m
@@ -0,0 +1,15 @@
+function [ data ] = getAllData( obj )
+%GETALLDATA Get all the image data
+%   This method extracts all the image data from an LSMReader object
+%
+% AUTHOR: Stefano Masneri
+% Date: 08.02.2017
+
+progBar = TextProgressBar('LSMReader --> Extracting data: ', 30);
+
+data = zeros(obj.height, obj.width, obj.channels, obj.stacks, obj.time, obj.datatype);
+
+numSteps = obj.Channels * obj.stacks * obj.time;
+
+end
+
