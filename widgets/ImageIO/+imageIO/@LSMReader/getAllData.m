1d4d0048ff18f0c9563f2fbcae0f08cdbdee0a17
Masneri Stefano <stocastico@gmail.com>
2017-02-10 15:06:53 +0100

    complete getAllData for LSMReader objs. Must test

--- widgets/ImageIO/+imageIO/@LSMReader/getAllData.m
+++ widgets/ImageIO/+imageIO/@LSMReader/getAllData.m
@@ -12,7 +12,50 @@ progBar = TextProgressBar('LSMReader --> Extracting data: ', 30);
 
 data = zeros(obj.height, obj.width, obj.channels, obj.stacks, obj.time, obj.datatype);
 
-numSteps = obj.Channels * obj.stacks * obj.time;
+numSteps = obj.channels * obj.stacks * obj.time * obj.numTilesRow * obj.numTilesCol;
+incr = 1;
+typeOut = str2func(obj.datatype);
+
+for indT = 1:obj.time
+  for indZ = 1:obj.stacks
+    for row = 1:obj.numTilesRow
+      for col = 1:obj.numTilesCol
+
+        %seek to beginning of current tile
+        tilePos = indT + (indZ-1)*(obj.time) + (col-1)*obj.stacks*obj.time + ...
+          (row-1)*obj.stacks*obj.time*numTilesCol;
+        fseek(obj.lsmPtr, obj.offsets(tilePos), 'bof');
+
+        %read data
+        for indC = 1:obj.channels
+          
+          progBar.update(incr/numSteps * 100);
+          incr = incr + 1;
+          
+          tmpImg = typeOut(fread(obj.lsmPtr, obj.pixPerTileRow * obj.pixPerTileCol, ...
+            obj.datatypeInput, obj.byteOrder));
+          
+          % Manage overlap
+          if 1 ~= row
+            ovDiffRow = round(obj.tileOverlap * obj.pixPerTileRow);
+          else
+            ovDiffRow = 0;
+          end
+          if 1 ~= col
+            ovDiffCol = round(obj.tileOverlap * obj.pixPerTileCol);
+          else
+            ovDiffCol = 0;
+          end
+          startR = 1 + (row - 1) * (obj.pixPerTileRow - ovDiffRow);
+          startC = 1 + (col - 1) * (obj.pixPerTileCol - ovDiffCol);
+          endR   = startR + obj.pixPerTileRow - 1;
+          endC   = startC + obj.pixPerTileCol - 1;
+          data(startR:endR, startC:endC, C, Z, T, S) = tmpImg;
+        end
+      end
+    end
+  end
+end
 
 end
 
