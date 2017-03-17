4ae9ef00a443341881e9e2ca0db75937e211b4e9
Masneri Stefano <stocastico@gmail.com>
2017-03-17 11:15:51 +0100

    Complete CZI metadata extraction

--- widgets/ImageIO/+imageIO/@CZIReader/readRawMetadataSegm.m
+++ widgets/ImageIO/+imageIO/@CZIReader/readRawMetadataSegm.m
@@ -15,10 +15,10 @@ function obj = readRawMetadataSegm( obj )
 
   % Read xml
   xmlData = fread(obj.cziPtr, xmlSize, '*char')';
-  obj.originalMetadata = xmlData;
   
   % Convert to struct
   metadataStruct = xml2struct(xmlData);
+  obj.originalMetadata = metadataStruct;
   
   % Now we have an incredibly nested structure. go through all the fields
   % and try to extract all the metadata info from it
@@ -38,35 +38,116 @@ function obj = readRawMetadataSegm( obj )
   end
   
   try
+    obj.objectiveName = microscopeInfo.Objectives.Objective.Manufacturer.Model.Text;
+  catch
+    disp('Objective name not available')
+  end
+  
+  try
     obj.NA = str2double(microscopeInfo.Objectives.Objective.LensNA.Text);
   catch
     disp('Numerical aperture info not available')
   end
+  
+  try
+    obj.objectiveMagnification = str2double(microscopeInfo.Objectives.Objective.NominalMagnification.Text);
+  catch
+    disp('Objective Magnification info not available')
+  end
 
   try
-    obj.refractionMedia = microscopeInfo.Objectives.Objective.Immersion.Text;
+    obj.refractiveMedium = microscopeInfo.Objectives.Objective.Immersion.Text;
+    if strcmpi(obj.refractiveMedium, 'Air')
+      obj.refractiveIndex = 1;
+    elseif strcmpi(obj.refractiveMedium, 'W') || strcmpi(obj.refractionMedium, 'Water')
+      obj.refractiveIndex = 1.33;
+    elseif strcmpi(obj.refractiveMedium, 'Oil')
+      obj.refractiveIndex = 1.5;
+    end
   catch
     disp('Refraction media info not available')
   end
+  
+  
+
+%   try
+%     % we don't know which light source is used... so we don't know the name
+%     % of the structure either!
+%     obj.wavelengthExc = [];
+%     lightSrc = microscopeInfo.LightSources.LightSource;
+%     for k = 1:length(lightSrc)
+%     	lst = lightSrc{k}.LightSourceType;
+%       fn = fieldnames(lst);
+%       obj.wavelengthExc = [obj.wavelengthExc, str2double(lst.(fn{1}).Wavelength.Text)];
+%     end
+%   catch
+%     disp('Excitation wavelength info not available')
+%   end
 
   try
-    % we don't know which light source is used... so we don't know the name
-    % of the structure either!
-    obj.wavelengthExc = [];
-    lightSrc = microscopeInfo.LightSources.LightSource;
-    for k = 1:length(lightSrc)
-    	lst = lightSrc{k}.LightSourceType;
-      fn = fieldnames(lst);
-      obj.wavelengthExc = [obj.wavelengthExc, str2double(lst.(fn{1}).Wavelength.Text)];
+    ChanInfo = top.Information.Image.Dimensions.Channels.Channel;
+    numCh = length(ChanInfo);
+    obj.wavelengthExc = cell(1, numCh);
+    obj.wavelengthEm = cell(1, numCh);
+    obj.zoom = cell(1, numCh);
+    obj.gain = nan(1, numCh);
+    obj.timePixel = nan(1, numCh);
+    obj.timeLine = nan(1, numCh);
+    obj.timeFrame = nan(1, numCh);
+    obj.timeStack = nan(1, numCh);
+    for k = 1:length(ChanInfo)
+      if iscell(ChanInfo)
+        currChan = ChanInfo{k};
+      else
+        currChan = ChanInfo;
+      end
+      try
+        obj.wavelengthExc{k} = str2num(currChan.ExcitationWavelength.Text);
+      catch
+        obj.wavelengthExc{k} = nan;
+      end
+      try
+        obj.wavelengthEm{k} = str2num(currChan.EmissionWavelength.Text);
+      catch
+        obj.wavelengthEm{k} = nan;
+      end
+      try
+        obj.wavelengthEm{k} = str2double(currChan.EmissionWavelength.Text);
+      catch
+      end
+      try
+        obj.timePixel(k) = str2double(currChan.LaserScanInfo.PixelTime.Text);
+      catch
+      end
+      try
+        obj.timeLine(k) = str2double(currChan.LaserScanInfo.LineTime.Text);
+      catch
+      end
+      try
+        obj.timeFrame{k} = str2double(currChan.LaserScanInfo.FrameTime.Text);
+      catch
+      end
+      try
+        obj.timeStack{k} = str2double(currChan.LaserScanInfo.StackTime.Text);
+      catch
+      end
+      try
+        obj.zoom{k} = [str2double(currChan.LaserScanInfo.ZoomX.Text), ...
+                       str2double(currChan.LaserScanInfo.ZoomX.Text)];
+      catch
+        obj.zoom{k} = nan;
+      end
+      try
+        obj.gain(k) = str2double(currChan.DetectorSettings.Gain.Text);
+      catch
+      end
+    end
+    if 1 == length(ChanInfo)
+      obj.zoom = obj.zoom{1};
+      obj.wavelengthEm = obj.wavelengthEm{1};
+      obj.wavelengthExc = obj.wavelengthExc{1};
     end
   catch
-    disp('Excitation wavelength info not available')
-  end
-
-  try
-    % info stored in Instrument.Detectors.Detector ???
-  catch
-    disp('Zoom and Gain info not available')
   end
   
   % Image info
@@ -149,7 +230,40 @@ function obj = readRawMetadataSegm( obj )
     obj.tileOverlap = 0;
   end
   
-  %% The field "DisplaySetting" has info related to the Channels
+  % Laser power
+  try
+    mts = top.Experiment.ExperimentBlocks.AcquisitionBlock.MultiTrackSetup.TrackSetup;
+    obj.laserPower = cell(1, length(mts));
+    for k = 1:length(mts)
+      try
+        if iscell(mts)
+          transmissions = mts{k}.Attenuators.Attenuator.Transmissions.Transmission;
+        else
+           transmissions = mts.Attenuators.Attenuator.Transmissions.Transmission;
+        end
+        if length(transmissions) == 1
+          obj.laserPower{k} = [num2str(100*str2double(transmissions.Text)) '%'];
+        else
+          obj.laserPower{k} = [num2str(100*str2double(transmissions{1}.Text)) '% - ' ...
+                            num2str(100*str2double(transmissions{end}.Text)) '%'];
+        end
+      catch % only one value of power
+        if iscell(mts)
+          transmission = mts{k}.Attenuators.Attenuator.Transmission;
+        else
+          transmission = mts.Attenuators.Attenuator.Transmission;
+        end
+        obj.laserPower{k} = [num2str(100*str2double(transmission.Text)) '%'];
+      end
+    end
+    if length(obj.laserPower) == 1
+      obj.laserPower = obj.laserPower{1};
+    end
+  catch
+    disp('Laser Power info not available');
+  end
+  
+  % The field "DisplaySetting" has info related to the Channels
   ch = top.DisplaySetting.Channels.Channel;
   if isstruct(ch)
     ch = {ch};
@@ -157,6 +271,7 @@ function obj = readRawMetadataSegm( obj )
   for k = 1:length(ch) %check all channels
     obj.channelInfo = [obj.channelInfo, ChannelInfo(ch{k}, 'CZI')];
   end
+  
   
   % The field "Scaling" contain info about the pixels physical size
   scale = top.Scaling.Items.Distance;
@@ -172,5 +287,6 @@ function obj = readRawMetadataSegm( obj )
       otherwise
         warning('CZIReader.readRawMetadataSegm: unrecognized dimension for scale')
     end
+    obj.scaleUnits = {'m', 'm', 'm'};
   end
 end
