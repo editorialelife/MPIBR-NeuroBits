069ff84d673b717d887d20512859607c4f066b15
Masneri Stefano <stocastico@gmail.com>
2017-03-22 09:15:21 +0100

    Bump version

--- widgets/WidgetImageBrowser/submodules/ImageIO/+imageIO/getVersion.m
+++ widgets/WidgetImageBrowser/submodules/ImageIO/+imageIO/getVersion.m
@@ -1,9 +1,11 @@
-function [ version, major, minor ] = getVersion()
+function [ version, major, minor, maintenance ] = getVersion()
 %GETVERSION Return the current version of the toolbox
 
 major = 1;
 minor = 0;
-version = [num2str(major), '.' num2str(minor)];
+maintenance = 1;
+
+version = [num2str(major), '.' num2str(minor). num2str(maintenance)];
 
 end
 
