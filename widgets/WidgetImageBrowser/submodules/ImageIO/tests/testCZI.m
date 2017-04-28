374e560ba47eb092bca8805ea942c70bb291818c
Georgi Tushev <georgi.handzhiyski@gmail.com>
2017-04-28 14:56:04 +0200

    update CZIReader tests

--- widgets/WidgetImageBrowser/submodules/ImageIO/tests/testCZI.m
+++ widgets/WidgetImageBrowser/submodules/ImageIO/tests/testCZI.m
@@ -1,4 +1,11 @@
 % TEST CZI FILES
+clc
+clear variables
+close all
+
+%% add path
+path_project = '../';
+addpath(genpath(path_project));
 
 %% INPUT DATA
 if ispc
@@ -7,68 +14,117 @@ else
   cziFolder = '/Volumes/data/Projects/ImageIO/TestDataFormats/ZeissCZI';
 end
 
-%% TEST CZI DATA - READ ALL DATA
-cziFiles = {'2Positions.czi', '2x2Tiles.czi', '5Z.czi', '5Z10T2x2Tiles2Pos.czi', ...
-  '10T.czi', 'Slide02_10x_RGT_Shading Correction.czi', ...
-  'Slide03_10x_RGT_Shading Correction_StitchZenBlue.czi', ...
-  ['ProblematicFiles' filesep() 'GreenSlide_Stack_1p-405_2p-720_exc561.czi']};
-
-%% EXAMPLE FILES - READ ALL THE DATA
-for k = 1:5
-  filename = cziFiles{k};
-  fullPath = fullfile(cziFolder, filename);
-  cziFile = imageIO.CZIReader(fullPath);
-  cziData = cziFile.read();
-  if ismatrix(cziData)
-    imshow(imadjust(cziData))
-    pause(0.5)
-  elseif 3 == ndims(cziData)
-    for m = 1:size(cziData, 3)
-      imshow(imadjust(cziData(:,:,m)))
-      pause(0.5)
+%% files to test
+cziTests = {'2Positions.czi','readFullData';...
+            '2x2Tiles.czi','readFullData';...
+            '5Z.czi','readFullData';...
+            '5Z10T2x2Tiles2Pos.czi','readFullData';...
+            '10T.czi','readFullData';...
+            'Slide02_10x_RGT_Shading Correction.czi','readSubsampleByFactor';...
+            'Slide03_10x_RGT_Shading Correction_StitchZenBlue.czi','readFullWithWrongMetadata';...
+            ['ProblematicFiles', filesep(), 'GreenSlide_Stack_1p-405_2p-720_exc561.czi'],'readNZSlices';...
+            ['ProblematicFiles2', filesep(), '01_FOV01_20mW_05s_09spotsIn07planes.czi'],'readSegments';...
+            ['ProblematicFiles2', filesep(), '02_FOV02_20mW_05s_09spotsIn07planes_afterHydrogelCalibration.czi'],'readSegments';...
+            };
+cziTestsCount = size(cziTests, 1);
+            
+%% test each file
+f = 10;
+for f = 1 : cziTestsCount
+    
+    % current file and test
+    currentFile = fullfile(cziFolder,cziTests{f,1});
+    currentTest = cziTests{f,2};
+    fprintf('### current test:\t%s\n',currentTest);
+    fprintf('### file:\t%s\n',currentFile);
+    
+    % create ImageIO object
+    cziFile = imageIO.CZIReader(currentFile);
+    
+    % take action related to current test
+    switch currentTest
+        case 'readFullData'
+            
+            % read full data structure
+            cziData = cziFile.read();
+            
+            % show image
+            hf = figure('color','w');
+            if ismatrix(cziData)
+                imshow(imadjust(cziData));
+            else
+                czi_size_dims = size(cziData);
+                for d = 1 : czi_size_dims(end)
+                    
+                    switch ndims(cziData)
+                        case 3
+                            imshow(imadjust(cziData(:,:,d)));
+                        case 4
+                            imshow(imadjust(cziData(:,:,1,d)));
+                        case 5
+                            imshow(imadjust(cziData(:,:,1,1,d)));
+                        case 6
+                            imshow(imadjust(cziData(:,:,1,1,1,d)));
+                    end
+                    
+                    pause(0.5);
+            
+                end
+            end
+            close(hf);
+         
+        case 'readSubsampleByFactor'
+            
+            %extract only one channel, subsample by a factor 2
+            cziData = cziFile.read('Rows', 1:2:cziFile.pixPerTileRow, 'Cols', 1:2:cziFile.pixPerTileCol, 'C', 1);
+            
+            % show image
+            hf = figure('color','w');
+            for d = 1:size(cziData, 3)
+              imshow(imadjust(cziData(:,:,d)))
+              pause(0.5)
+            end
+            close(hf);
+            
+        case 'readFullWithWrongMetadata'    
+            cziData = cziFile.read();  % read all, cannot read only partially if wrongMetadata is true
+            
+            % show image
+            hf = figure('color','w');
+            for d = 1:size(cziData, 3) % display three channels, second series
+              imshow(imadjust(cziData(:,:,d)));
+              pause(0.5);
+            end
+            close(hf);
+        
+        case 'readNZSlices'
+            
+            cziData = cziFile.read('Z', 1:20); %extract only 20 slices
+            
+            % show image
+            hf = figure('color','w');
+            for d = 1:size(cziData, 3)
+              imshow(imadjust(cziData(:,:,d)));
+              pause(0.5);
+            end
+            close(hf);
+        
+        case 'readSegments'
+            
+            cziData = cziFile.read();
+            hf = figure('color','w');
+            imshow(mean(cziData,3),[]);
+            pause(1.0);
+            close(hf);
+            
+        otherwise
+            warning('unrecognized test type');
     end
-  elseif 4 == ndims(cziData)
-    for m = 1:size(cziData, 4)
-      imshow(imadjust(cziData(:,:,1,m)))
-      pause(0.5)
-    end
-  elseif 5 == ndims(cziData)
-    for m = 1:size(cziData, 5)
-      imshow(imadjust(cziData(:,:,1,1,m)))
-      pause(0.5)
-    end
-  else % 6D
-    for m = 1:size(cziData, 6)
-      imshow(imadjust(cziData(:,:,1,1,m)))
-      pause(0.5)
-    end
-  end  
+    
+    % free ImageIO object
+    cziFile.delete();
 end
 
-%% OTHER FILES - GET A SUBSET OF THE DATA
-for k = 6:length(cziFiles)
-  filename = cziFiles{k};
-  fullPath = fullfile(cziFolder, filename);
-  cziFile = imageIO.CZIReader(fullPath);
-  if 6 == k
-    %extract only one channel, subsample by a factor 2
-    cziData = cziFile.read('Rows', 1:2:cziFile.pixPerTileRow, 'Cols', 1:2:cziFile.pixPerTileCol, 'C', 1);
-    for m = 1:size(cziData, 3)
-      imshow(imadjust(cziData(:,:,m)))
-      pause(0.5)
-    end
-  elseif 7 == k
-    cziData = cziFile.read();  % read all, cannot read only partially if wrongMetadata is true
-    for m = 1:size(cziData, 3) % display three channels, second series
-      imshow(imadjust(cziData(:,:,m, 1, 1, 2)))
-      pause(0.5)
-    end
-  else
-    %extract only 20 slices
-    cziData = cziFile.read('Z', 1:20);
-    for m = 1:size(cziData, 3)
-      imshow(imadjust(cziData(:,:,m)))
-      pause(0.2)
-    end
-  end
-end
+
+%% clean path
+rmpath(genpath(path_project));
