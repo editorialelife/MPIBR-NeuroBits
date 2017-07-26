c6cdbf1d7ad89812530a08e00f4085ffaba81c4b
Georgi Tushev <georgi.handzhiyski@gmail.com>
2017-07-26 16:48:11 +0200

    add mvc branch

--- utils/ind2sub2D.m
+++
@@ -1,9 +0,0 @@
-function [r, c] = ind2sub2D(siz, idx)
-% faster ind2sub2D
-% siz(1) = nrows
-% siz(2) = ncols
-
-    r = rem(idx-1, siz(1)) + 1;
-    c = (idx - r) / siz(1) + 1;
-end
-
--- utils/sub2ind2D.m
+++
@@ -1,7 +0,0 @@
-function [idx] = sub2ind2D(siz, rows, cols)
-% a fast replacement for sub2ind in 2D
-
-idx = rows + (cols - 1) * siz(1);
-
-end
-
