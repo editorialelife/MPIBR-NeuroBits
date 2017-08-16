0db945fbc487b477ae795f86fc21b715fca528ba
Georgi Tushev <georgi.handzhiyski@gmail.com>
2017-08-16 11:05:18 +0200

    add test case

---
+++ widgets/WidgetNeuroTree/test/test.m
@@ -0,0 +1,15 @@
+%% WidgetNeuroTree
+% test script and example usage
+%
+% Aug 2017
+% 
+clc
+clear variables
+close all
+
+addpath([pwd,filesep,'..',filesep]);
+
+
+%% test callbacks on figure
+obj = WidgetNeuroTree();
+    
